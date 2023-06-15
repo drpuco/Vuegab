61d6ef73206eff8b8db3fa34b9c40945fa45cb18
Honk2 <honkderzweite@gmail.com>
2023-06-15 11:19:34 +0200

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-toolbar
+      dark
+      prominent
+      image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-app-bar-nav-icon white></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="text-white">GAB Bildung</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-spacer />
+    <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank"> 
+    <span class="mr-2">Neue Website</span>
+    </v-btn>
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+    <v-footer class="bg-grey-lighten-1">
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        variant="text"
+        class="mx-2"
+        rounded="xl"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col class="text-center mt-4" cols="12">
+        {{ new Date().getFullYear() }} — <strong>GAB Bildung</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -8,19 +47,13 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
